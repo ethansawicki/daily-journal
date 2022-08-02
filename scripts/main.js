37cada1587e67fd8558844069e76e714f633d2c8
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-01 20:14:22 -0500

    added some JS practice from school

---
+++ scripts/main.js
@@ -0,0 +1,65 @@
+const journalEntries = [
+    {
+        id: 1,
+        date: "08/01/2022",
+        concept: "Leonids Toys",
+        entry: "Completed Leonids Toys on this past Saturday (07/30/2022)",
+        mood: "Alive"
+    },
+    {
+        id: 2,
+        date: "08/01/2022",
+        concept: "New Groups",
+        entry: "We got new groups this past Saturday (07/30/2022)",
+        mood: "Sadge"
+    },
+    {
+        id: 3,
+        date: "08/01/2022",
+        concept: "Test 3",
+        entry: "Blank",
+        mood: "Happy"
+    },
+    {
+        id: 4,
+        date: "08/01/2022",
+        concept: "Test 4",
+        entry: "Blank",
+        mood: "Alive"
+    },
+    {
+        id: 5,
+        date: "08/01/2022",
+        concept: "Test 5",
+        entry: "Blank",
+        mood: "Alive"
+    },
+    {
+        id: 6,
+        date: "08/01/2022",
+        concept: "Test 6",
+        entry: "Blank",
+        mood: "Alive"
+    },
+    {
+        id: 7,
+        date: "08/01/2022",
+        concept: "Test 7",
+        entry: "Blank",
+        mood: "Alive"
+    }
+]
+
+for (const entries of journalEntries) {
+    console.log(entries.entry)
+}
+
+for (const entries of journalEntries) {
+    console.log(entries.concept)
+}
+
+for (const entries of journalEntries) {
+    if (entries.id % 2 !== 0) {
+        console.log(entries.id)
+    }
+}
